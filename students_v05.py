98125813071d63a7693d47391015686cb129ca9b
pixelpip <rajimoto@gmail.com>
2023-04-17 19:31:46 -0700

    Delete students_v05.py

--- students_v05.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-
-name = input("What's your name? ")
-home = input("where's your home? ")
-
-with open("names.csv", "a", newline='') as file:
-    writer = csv.DictWriter(file, fieldnames=["name", "home"])
-    writer.writerow({"name": name, "home": home})
